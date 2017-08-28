763c393ec3a8248512a6dd17f4d5a40facd51a27
Mathieu Victor Guerin <GueMa1630770@cegepjonquiere.ca>
2017-08-28 16:54:56 -0400

    Gestion du text dans Intro

---
+++ Assets/sliderInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class sliderInteraction : MonoBehaviour {
+
+    public Text sliderValue;
+    public Slider slider;
+
+	void Update () {
+
+        sliderValue.text = slider.value.ToString() + " Player(s)";
+                
+	}
+}
